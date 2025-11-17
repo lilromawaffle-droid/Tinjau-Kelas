59c619fa452f5e573ef6f44b4a9204c92b649f86
Akmal Khairan Rizaldi <akmalcode843@gmail.com>
2025-11-17 08:11:08 +0700

    cardview dependcies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 
 dependencies {
     implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
